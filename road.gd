be74f170b9f9c5008436f4ae8c1a142e21b91596
JoannaGoj <joannagojan@gmail.com>
2024-11-09 13:34:02 +0800

    level

--- road.gd
+++ road.gd
@@ -1,14 +1,16 @@
 extends Node2D
-
+var direction
+var speed = 400
+var velocity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	velocity = move_and_slide(speed, Vector2.Up)
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
