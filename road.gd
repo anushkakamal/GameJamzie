d603961f01ad0b793a71d9d37015fdddbfcd9379
JoannaGoj <joannagojan@gmail.com>
2024-11-09 13:16:35 +0800

    road initial

--- road.gd
+++ road.gd
@@ -1,8 +1,6 @@
-extends Node
-var spawn_position = $Marker2D
- 
+extends Node2D
 
-#hi bye awesome done
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
