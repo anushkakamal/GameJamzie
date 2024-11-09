4a2c45189ae466594374d363712d1091fc9634e9
JoannaGoj <joannagojan@gmail.com>
2024-11-09 14:31:54 +0800

    road moving

--- road.gd
+++ road.gd
@@ -3,6 +3,9 @@ var direction
 var speed = 400
 var velocity
 var collision_y: int
+var jump_left : bool
+var jump_right : bool
+var jump_time = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +15,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Road.position.y += 1
+	if event is InputEventMouse:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			print('press')
+			
 	if $"Road/Sprite2D - obstacle".position.y == collision_y:
 		print('collis')
 	
